aac4e619f0d791e90bd4d55656562d9c3a85bfab
Shohidullah Kaisar <ravenkaisar@gmail.com>
2023-10-20 23:36:25 +0600

    fix: API base url add to react service

--- services/react/src/App.js
+++ services/react/src/App.js
@@ -1,35 +1,35 @@
 import './App.css';
 import * as md5 from 'js-md5'
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import Form from "./Components/Form";
-import {toast, ToastContainer} from 'react-toastify';
+import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const baseURL = "http://localhost:6006/api/v1/customers";
+const baseURL = process.env.API_BASE_URL || "http://localhost:8000/api";
 
 function App() {
     const [customers, setCustomers] = useState([]);
     const [isSubmitted, setIsSubmitted] = useState(false)
-    function handleSubmission (){setIsSubmitted(!isSubmitted)}
+    function handleSubmission() { setIsSubmitted(!isSubmitted) }
 
     const handleDelete = (id) => {
         // eslint-disable-next-line no-restricted-globals
         if (confirm('Are you sure delete the customer ?')) {
-            axios.delete(`${baseURL}/${id}`)
+            axios.delete(`${baseURL}/v1/customers/${id}`)
                 .then((response) => {
-                    toast("Successfully delete the customer info", {type: "success"})
+                    toast("Successfully delete the customer info", { type: "success" })
                     setCustomers(customers.filter((customer) => customer.id != id))
                 })
                 .catch((error) => {
-                    toast("Something went wrong", {type: "error"})
+                    toast("Something went wrong", { type: "error" })
                 })
         }
     }
     useEffect(() => {
-        axios.get(baseURL)
+        axios.get(`${baseURL}/v1/customers`)
             .then((response) => {
-                toast(`Customer collection get from ${response.data.cached ? "redis" : "database"}`, {type: "info"})
+                toast(`Customer collection get from database`, { type: "info" })
                 setCustomers(response.data.data)
             })
     }, [isSubmitted])
@@ -47,22 +47,22 @@ function App() {
                 draggable
                 pauseOnHover
                 theme="light">
-                </ToastContainer>
+            </ToastContainer>
             <div className="relative isolate px-6 pt-1 lg:px-8">
                 <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-                     aria-hidden="true">
+                    aria-hidden="true">
                 </div>
                 <div className="mx-auto max-w-2xl py-2 sm:py-48 lg:py-8">
                     <div className="text-center">
                         <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-2xl">Customer Info</h1>
                     </div>
-                    <Form handleSubmission={handleSubmission}/>
+                    <Form handleSubmission={handleSubmission} />
                     <ul role="list" className="divide-y divide-gray-100">
                         {customers.map((person) => (<li key={person.id} className="flex justify-between gap-x-6 py-5">
                             <div className="flex gap-x-4">
                                 <img className="h-12 w-12 flex-none rounded-full bg-gray-50"
-                                     src={"https://www.gravatar.com/avatar/" + md5(person.email)}
-                                     alt=""/>
+                                    src={"https://www.gravatar.com/avatar/" + md5(person.email)}
+                                    alt="" />
                                 <div className="min-w-0 flex-auto">
                                     <p className="text-sm font-semibold leading-6 text-gray-900">{person.name}</p>
                                     <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.email}</p>
